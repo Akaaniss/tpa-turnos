8d6854594d3f72083096af4a4b651991ef3ff830
Akaaniss <annaismonserrat.skz@gmail.com>
2023-07-07 19:23:38 -0400

    Revert "update turnos"
    This reverts commit 1f6cd6b1fcf8244f6290b924f1a2adfa8e4754cb.

--- TPA - CODIGO.py
+++ TPA - CODIGO.py
@@ -2,8 +2,6 @@ import sys
 import csv
 from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget, QLineEdit, QPushButton, QTableWidgetItem, QTableWidget, QDateEdit, QComboBox, QMessageBox
 from PyQt6.QtCore import Qt, QDate
-
-
 # Ventana de Registro
 class RegistroWindow(QWidget):
     def __init__(self):
@@ -11,53 +9,28 @@ class RegistroWindow(QWidget):
         self.setWindowTitle("Registro")
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
-
         # Campos de entrada de datos para el registro
         self.username_input = QLineEdit()
         self.layout.addWidget(QLabel("Usuario:"))
         self.layout.addWidget(self.username_input)
-
         self.password_input = QLineEdit()
         self.password_input.setEchoMode(QLineEdit.EchoMode.Password)
         self.layout.addWidget(QLabel("Contraseña:"))
         self.layout.addWidget(self.password_input)
-
         # Botón para registrar una cuenta
         self.registrar_button = QPushButton("Registrarse")
         self.registrar_button.clicked.connect(self.registrar_cuenta)
         self.layout.addWidget(self.registrar_button)
-
     def registrar_cuenta(self):
         username = self.username_input.text()
         password = self.password_input.text()
+        # Verificar si el archivo "registro_de_cuentas.csv" existe
 
         # Guardar los datos de registro en el archivo CSV
+        with open("registro_de_cuentas.csv", "a") as file:
         with open("registro_de_cuentas.csv", "a", newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
             writer.writerow([username, password])
-
-        QMessageBox.information(self, "Registro exitoso", "La cuenta ha sido registrada con éxito")
-        self.close()
-
-# Ventana del Gerente
-class GerenteWindow(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Ventana del Gerente")
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(QLabel("¡Bienvenido, Gerente!"))
-        self.setLayout(self.layout)
-
-# Ventana del Ejecutivo de Viaje
-class EjecutivoWindow(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Ventana del Ejecutivo de Viaje")
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(QLabel("¡Bienvenido, Ejecutivo de Viaje!"))
-        self.setLayout(self.layout)
-
-# Ventana del Encargado de Logística
 class LogisticaWindow(QWidget):
     def __init__(self):
         super().__init__()
@@ -68,22 +41,14 @@ class LogisticaWindow(QWidget):
 
         # Crear una tabla para mostrar los turnos de los guías
         self.guia_table = QTableWidget()
+        self.guia_table.setColumnCount(5)
+        self.guia_table.setHorizontalHeaderLabels(["Nombre", "RUT", "Fecha", "Plan", "Turno"])
         self.guia_table.setColumnCount(6)
         self.guia_table.setHorizontalHeaderLabels(["Nombre", "RUT", "Fecha", "Plan", "Turno",'Eliminar'])
         self.layout.addWidget(self.guia_table)
 
         # Campos de entrada de datos
-        self.nombre_input = QLineEdit()
-        self.rut_input = QLineEdit()
-        self.fecha_input = QDateEdit()
-        self.fecha_input.setDate(QDate.currentDate())
-        self.plan_input = QComboBox()
-        self.plan_input.addItems(["Plan A", "Plan B", "Plan C"])
-        self.turno_input = QComboBox()
-        self.turno_input.addItems(["Mañana", "Tarde"])
-
-        # Botón para agregar un turno
-        self.agregar_button = QPushButton("Agregar Turno")
+def __init__(self):
         self.agregar_button.clicked.connect(self.agregar_turno)
         self.layout.addWidget(self.agregar_button)
 
@@ -99,7 +64,7 @@ class LogisticaWindow(QWidget):
         self.layout.addWidget(QLabel("Turno:"))
         self.layout.addWidget(self.turno_input)
         self.layout.addWidget(self.agregar_button)
-        
+
         # Cargar los turnos existentes desde el archivo CSV
         self.cargar_turnos()
         self.boton_eliminar()
@@ -109,6 +74,7 @@ class LogisticaWindow(QWidget):
 
         # Verificar si el archivo "registro_de_cuentas.csv" existe
         try:
+            with open("registro_de_cuentas.csv", "r") as file:
             with open("turnos.csv", "r",newline='', encoding='utf-8') as file:
                 reader = csv.reader(file)
                 for row in reader:
@@ -117,7 +83,8 @@ class LogisticaWindow(QWidget):
                         self.guia_table.setItem(self.guia_table.rowCount() - 1, i, QTableWidgetItem(item))
         except FileNotFoundError:
             pass
-        
+
+
     def boton_eliminar(self):
         i = 1
         with open('turnos.csv', newline='') as file:
@@ -128,7 +95,7 @@ class LogisticaWindow(QWidget):
                     delete_button.clicked.connect(self.eliminar)
                     self.guia_table.setCellWidget(self.guia_table.rowCount() - i , 5, delete_button)
                 i += 1
-    
+
     def agregar_turno(self):
         nombre = self.nombre_input.text()
         rut = self.rut_input.text()
@@ -137,7 +104,7 @@ class LogisticaWindow(QWidget):
         turno = self.turno_input.currentText()
         boton_eliminar = QPushButton('Eliminar')
         boton_eliminar.clicked.connect(self.eliminar)
-        
+
         if nombre != '' and rut != '':
             with open('turnos.csv', 'a', newline='', encoding='utf-8') as file:
                 writer = csv.writer(file)
@@ -182,42 +149,13 @@ class LogisticaWindow(QWidget):
 
 
 # Ventana de inicio de sesión
-class InicioSesionWindow(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Inicio de Sesión")
-        self.layout = QVBoxLayout()
-        self.setLayout(self.layout)
-
-        # Campos de entrada de usuario y contraseña
-        self.username_input = QLineEdit()
-        self.layout.addWidget(QLabel("Usuario:"))
-        self.layout.addWidget(self.username_input)
-
-        self.password_input = QLineEdit()
-        self.password_input.setEchoMode(QLineEdit.EchoMode.Password)
-        self.layout.addWidget(QLabel("Contraseña:"))
-        self.layout.addWidget(self.password_input)
-
-        # Botón de inicio de sesión
-        self.login_button = QPushButton("Iniciar Sesión")
-        self.login_button.clicked.connect(self.login)
-        self.layout.addWidget(self.login_button)
-
-        # Botón de registro
-        self.register_button = QPushButton("Registrarse")
-        self.register_button.clicked.connect(self.open_registro_window)
-        self.layout.addWidget(self.register_button)
-
+class TurnosApp(QMainWindow):
     def login(self):
-        username = self.username_input.text()
-        password = self.password_input.text()
-
-        # Verificar si el usuario y contraseña coinciden con los registros
-        with open('registro_de_cuentas.csv', newline='') as cuentas:
+        with open('registro_de_cuentas.csv',newline='') as cuentas:
             reader = csv.DictReader(cuentas)
             for row in reader:
-                if username == row['Usuario'] and password == row['Contraseña']:
+                print(row)
+                if username == row['logistica'] and password == row['abcd']:
                     print("Inicio de sesión exitoso")
                     self.open_logistica_window()
                     self.close()
@@ -229,57 +167,18 @@ class InicioSesionWindow(QWidget):
                 error_dialog.setText("Usuario o contraseña incorrectos.")
                 print("Inicio de sesión fallido")
                 error_dialog.exec()
-
-    def open_registro_window(self):
-        self.registro_window = RegistroWindow()
-        self.registro_window.show()
-
-
-class TurnosApp(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Turnos App")
-        self.resize(800, 600)
-
-        # Crear una barra de menú
-        menu_bar = self.menuBar()
-
-        # Crear un menú "Rol" con opciones para el Gerente, Ejecutivo de Viaje y Encargado de Logística
-        rol_menu = menu_bar.addMenu("Rol")
-
-        # Acción para abrir la ventana del Gerente
-        gerente_action = rol_menu.addAction("Gerente")
-        gerente_action.triggered.connect(self.open_gerente_window)
-
-        # Acción para abrir la ventana del Ejecutivo de Viaje
-        ejecutivo_action = rol_menu.addAction("Ejecutivo de Viaje")
-        ejecutivo_action.triggered.connect(self.open_ejecutivo_window)
-
-        # Acción para abrir la ventana del Encargado de Logística
-        logistica_action = rol_menu.addAction("Encargado de Logística")
-        logistica_action.triggered.connect(self.open_logistica_window)
-
-        # Crear la ventana de inicio de sesión
-        self.inicio_sesion_widget = InicioSesionWindow()
-        self.setCentralWidget(self.inicio_sesion_widget)
-
-    def open_inicio_sesion_window(self):
-        self.inicio_sesion_window = InicioSesionWindow()
-        self.inicio_sesion_window.show()
-
     def open_gerente_window(self):
         self.gerente_window = GerenteWindow()
         self.gerente_window.show()
-
     def open_ejecutivo_window(self):
         self.ejecutivo_window = EjecutivoWindow()
         self.ejecutivo_window.show()
-
     def open_logistica_window(self):
         self.logistica_window = LogisticaWindow()
         self.logistica_window.show()
-
-
+    def open_registro_window(self):
+        self.registro_window = RegistroWindow()
+        self.registro_window.show()
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = TurnosApp()
